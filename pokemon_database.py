0431990fade22ee2a47b0d732d58975032701a7d
Adrian <adrian2000alf@gmail.com>
2022-12-30 17:34:51 +0100

    Pokemon database created

--- pokemon_database.py
+++ pokemon_database.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 pokemon_path = "./Pokemons.xlsx"
 
-pokemon_df = pd.read_excel(pokemon_path, index_col=1)
-print(pokemon_df)
+pokemon_df = pd.read_excel(pokemon_path, index_col = 0)
+pokemon_df.drop('Página Web', axis = 1,inplace = True)
 
+pokemons = pokemon_df.to_dict('index')
+pokemons['Charizard']
